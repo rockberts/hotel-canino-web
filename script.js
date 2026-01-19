04a307de449967ffa2186c3ce396a60f32938912
rockberts <rockberts@gmail.com>
2026-01-19 11:10:58 -0500

    Add chatbot button functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hotel Canino Web cargado");
+const button = document.getElementById("chatbot-button");
+const windowChat = document.getElementById("chatbot-window");
+const closeBtn = document.getElementById("chatbot-close");
+
+button.onclick = () => {
+  windowChat.style.display = "flex";
+};
+
+closeBtn.onclick = () => {
+  windowChat.style.display = "none";
+};
